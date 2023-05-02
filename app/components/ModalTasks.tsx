033629d6084bc7ba2d95643998cffefcfd4a2cd1
hemant7io <hemant.mourya@cloudactivelabs.in>
2023-05-02 21:47:33 +0530

    mantine ui form

--- app/components/ModalTasks.tsx
+++ app/components/ModalTasks.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Manrope, Roboto } from "next/font/google";
-import { catergory } from "../utils/data";
+import { useForm } from "@mantine/form";
+import { TextInput, Select, Box, Button, Textarea } from "@mantine/core";
 interface typeModal {
   showModal: Boolean;
   handleCloseModal: any;
@@ -9,6 +10,20 @@ const manrope = Manrope({ subsets: ["latin"] });
 const roboto = Manrope({ weight: "400", subsets: ["latin"] });
 const ModalTasks = (props: typeModal) => {
   const { showModal, handleCloseModal } = props;
+  const formData = useForm({
+    initialValues: {
+      task: "",
+      project: "",
+      priority: "",
+      status: "",
+      task_type: "",
+      state_date: "",
+      end_date: "",
+      mileStone: "",
+      estimate_Time: "",
+      description: " ",
+    },
+  });
   return (
     <>
       {showModal && (
@@ -29,7 +44,7 @@ const ModalTasks = (props: typeModal) => {
                 </h2>
               </div>
               <div className="p-4 ">
-                <form className="grid grid-cols-2 place-content-center  ">
+                {/* <form className="grid grid-cols-2 place-content-center  ">
                   <div className="my-2 flex flex-col">
                     <label htmlFor="task" className="my-2 font-semibold">
                       Task
@@ -183,6 +198,80 @@ const ModalTasks = (props: typeModal) => {
                       Cancel
                     </button>
                   </div>
+                </form> */}
+                <form
+                  className="grid grid-cols-2 place-content-center  gap-4 "
+                  onSubmit={formData.onSubmit((values) => console.log(values))}
+                >
+                  <TextInput
+                    label="Task"
+                    placeholder="Enter your task name"
+                    {...formData.getInputProps("task")}
+                  />
+                  <TextInput
+                    label="Project"
+                    placeholder="Enter Project name"
+                    {...formData.getInputProps("project")}
+                  />
+                  <Box className="col-span-2 grid grid-cols-3 gap-5 place-content-center">
+                    <Select
+                      label="Priority"
+                      placeholder="priority"
+                      data={[
+                        { value: "urgent", label: "Urgent" },
+                        { value: "high", label: "High" },
+                        { value: "no priority", label: "No Priority" },
+                        { value: "backlog", label: "BackLog" },
+                      ]}
+                      {...formData.getInputProps("priority")}
+                    />
+                    <Select
+                      label="Status"
+                      placeholder="select status"
+                      data={[
+                        { value: "active", label: "active" },
+                        { value: "delay", label: "delay" },
+                        { value: "close", label: "close" },
+                      ]}
+                      {...formData.getInputProps("status")}
+                    />
+                    <Select
+                      label="Task type"
+                      placeholder="select task type"
+                      data={[
+                        { value: "frontend", label: "Frontend" },
+                        { value: "backend", label: "backend" },
+                        { value: "bug", label: "bug" },
+                      ]}
+                      {...formData.getInputProps("task_type")}
+                    />
+                  </Box>
+                  <TextInput
+                    label="MileStone"
+                    placeholder="mileStone"
+                    {...formData.getInputProps("mileStone")}
+                  />
+                  <TextInput
+                    label="Estimate Time"
+                    placeholder="Enter Estimate time"
+                    {...formData.getInputProps("estimate_Time")}
+                  />
+                  <Textarea
+                    label="Desciption"
+                    placeholder="Enter description"
+                    {...formData.getInputProps("description")}
+                  />
+                  <Box className="col-span-2  flex justify-center items-center">
+                    <Button type="submit" className="bg-blue-500">
+                      Save
+                    </Button>
+                    <Button
+                      className="px-4 py-2 mx-4 bg-blue-500 "
+                      onClick={handleCloseModal}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
                 </form>
               </div>
             </div>
